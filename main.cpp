a547d062e82ab8c2851eca8f44f3d47f1fcff6d5
johnlk <jlkuhn@purdue.edu>
2018-02-20 11:38:26 -0500

    added a main file for calling the compression command

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printHelp(){
+	cout << "Type huffman [-e, -d] inputFile outputFile" << endl;
+	cout << "-e is for encode -d is for decode" << endl;	
+}
+
+int main(int argc, char** argv){
+	if(argc < 4){
+		printHelp();
+		return 1;
+	}	
+	char mode = argv[1][1];
+	char *inFile = argv[2];
+	char *outFile = argv[3];
+
+	if(mode == 'e'){
+		cout << "encoding: " << inFile << endl;	
+	}else if(mode == 'd'){
+		cout << "decoding: " << inFile << endl;
+	}else{
+		printHelp();
+		return 1;
+	}
+	return 0;
+}
